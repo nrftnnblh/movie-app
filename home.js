ddb7798c40d5854ff8446e837141eabac168115f
nrftnnblh <99646465+nrftnnblh@users.noreply.github.com>
2022-02-15 14:01:47 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FirstHome = () =>
+	return (
+		<div
+			style= {{
+				display: 'flex';
+				justifyContect: 'Right';
+				alignItems: 'Right';
+				height: '100vh';
+			}}
+		>
+		
+			<h1>Movies</h1>
+		
+		</div>
+	);
+};
+
+export default FirstHome;
